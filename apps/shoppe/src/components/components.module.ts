d3ef181c5c85dbe9b0822e105519cd3326f01099
tttemurbek <nonemenoneme9999@gmail.com>
2025-04-08 01:04:28 +0900

    feat: create components related modules

--- apps/shoppe/src/components/components.module.ts
+++ apps/shoppe/src/components/components.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
-import { PropertyModule } from './property/property.module';
+import { JewelleryModule } from './jewellery/jewellery.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, PropertyModule]
+	imports: [
+		MemberModule,
+		AuthModule,
+		JewelleryModule,
+		BoardArticleModule,
+		LikeModule,
+		ViewModule,
+		CommentModule,
+		FollowModule,
+	],
 })
 export class ComponentsModule {}
