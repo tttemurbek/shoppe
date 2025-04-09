3b7407ebab818163f4ce41a47c41e465200e5285
tttemurbek <nonemenoneme9999@gmail.com>
2025-04-09 13:51:12 +0900

    feat: create checkAuthRoles

--- apps/shoppe/src/components/member/member.resolver.ts
+++ apps/shoppe/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { Member } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -41,6 +44,14 @@ export class MemberResolver {
 		return `Hi ${memberNick}, you are authenticated!`;
 	}
 
+	@Roles(MemberType.USER, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Query(() => String)
+	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+		console.log('Query checkAuthRoles called');
+		return `Hi ${authMember.memberNick}, you are authenticated, you are ${authMember.memberType}! (memberId: ${authMember._id})`;
+	}
+
 	@Query(() => String)
 	public async getMember(): Promise<string> {
 		console.log('Query getMember called');
@@ -49,7 +60,8 @@ export class MemberResolver {
 
 	/** ADMIN **/
 
-	// Authorization: ADMIN
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
 	@Mutation(() => String)
 	public async getAllMembersByAdmin(): Promise<string> {
 		return this.memberService.getAllMembersByAdmin();
