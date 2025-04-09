05a8b211b34e67ecbedfb11afe490c9022cf1787
tttemurbek <nonemenoneme9999@gmail.com>
2025-04-09 14:19:58 +0900

    fix: modify updateMember GraphQL apu

--- apps/shoppe/src/components/member/member.resolver.ts
+++ apps/shoppe/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -29,14 +30,6 @@ export class MemberResolver {
 	}
 
 	@UseGuards(AuthGuard)
-	@Mutation(() => String)
-	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-		console.log('Mutation updateMember called');
-		console.log(typeof memberId);
-		return this.memberService.updateMember();
-	}
-
-	@UseGuards(AuthGuard)
 	@Query(() => String)
 	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
 		console.log('Query checkAuth called');
@@ -50,6 +43,17 @@ export class MemberResolver {
 	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
 		console.log('Query checkAuthRoles called');
 		return `Hi ${authMember.memberNick}, you are authenticated, you are ${authMember.memberType}! (memberId: ${authMember._id})`;
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation updateMember called');
+		delete input._id;
+		return this.memberService.updateMember(memberId, input);
 	}
 
 	@Query(() => String)
